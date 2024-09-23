e8813a7d70750eb212b9c484134b0e772dcf90cf
dnschwarzer <57993658+dnschwarzer@users.noreply.github.com>
2024-09-23 12:33:29 +0200

    fix commonPipeline.groovy
    fix

--- vars/commonPipeline.groovy
+++ vars/commonPipeline.groovy
@@ -112,7 +112,7 @@ def buildDockerImage(String registryUrl, String classname, String dockerTag, Lis
             buildArgs.add("--build-arg ${arg}")
         }
         buildArgs.add("--build-arg GIT_API_KEY=${GIT_API_KEY}")
-        def argsString = "buildArgs.join(' ')
+        def argsString = buildArgs.join(' ')
         echo "ARG STRING: " + argsString
         return docker.build("${registryUrl}/cloudogu-backend/team-sos/${classname}:${dockerTag}", "--no-cache ${argsString} .")
     }
