610aef50b27a0f57ba0c08935c0f2f488cc6b331
dnschwarzer <57993658+dnschwarzer@users.noreply.github.com>
2025-02-28 10:09:30 +0100

    Update createTagAndDeploy.groovy
    gcloud command to list artifacts adjusted

--- vars/createTagAndDeploy.groovy
+++ vars/createTagAndDeploy.groovy
@@ -82,16 +82,7 @@ def call(Map config) {
                                                      "${registryUrl}/cloudogu-backend/team-${team}/${classname}/${subfolder}"
                 echo "Cleaning up repository: ${repoName}"
                 
-                // Choose the appropriate gcloud command based on the registry type
-                def listCmd = ""
-                if (registryUrl.contains("gcr.io")) {
-                    listCmd = "gcloud container images list-tags ${repoName} --format=json"
-                } else if (registryUrl.contains("pkg.dev")) {
-                    listCmd = "gcloud artifacts docker images list-tags ${repoName} --format=json"
-                } else {
-                    error("Registry ${registryUrl} not supported for cleanup")
-                }
-                
+                def listCmd = "gcloud container images list-tags ${repoName} --format=json"
                 // Authenticate with gcloud using the service account key
                 withCredentials([file(credentialsId: "ar-${team}-sf", variable: "GCLOUD_KEY_FILE")]) {
                     sh "gcloud auth activate-service-account --key-file=${GCLOUD_KEY_FILE}"
