26c43ea9d58b212f7633901a64280ac7eb82340a
dnschwarzer <57993658+dnschwarzer@users.noreply.github.com>
2025-12-09 11:52:45 +0100

    Fix library annotation syntax in createTagAndDeploy

--- vars/createTagAndDeploy.groovy
+++ vars/createTagAndDeploy.groovy
@@ -1,6 +1,6 @@
 // vars/createTagAndDeploy.groovy
 //@Library('cloudogu/gitops-build-lib@0.6.0')
-@Library(['github.com/cloudogu/ces-build-lib@4.1.1'])
+@Library('ces-build-lib@4.1.1') _
 import com.cloudogu.gitops.gitopsbuildlib.*
 import com.cloudogu.ces.cesbuildlib.*
 import java.util.Collections
